eaef0a621c8c875cd5d5f5eb13e473fbe366fda3
Abhigyanpaul1234 <92012565+Abhigyanpaul1234@users.noreply.github.com>
2023-05-05 20:31:31 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alerts from './components/Alerts';
+import About from './components/About';
 
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+
+ 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [Alert,setAlert]=useState(null)
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg: message
+      ,type:type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },4000)
+  }
+
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has  been enabled","success")
+      document.title='App-Dark Mode'
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled","success")
+      document.title='App-Light Mode'
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    {/* <Router> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alerts alert={Alert}/>
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" showAlert={showAlert} mode={mode}/>
+    {/* <Switch>
+          <Route exact path="/About">
+            <About />
+          </Route>
+          <Route exact path="/"> */}
+          {/* </Route>
+        </Switch> */}
+    {/* <About/> */}
     </div>
+    {/* </Router> */}
+    </> 
   );
 }
 
