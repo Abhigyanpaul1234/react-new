24ad8b682effd64aa73ee46978f5a9ba128f35b3
Abhigyanpaul1234 <92012565+Abhigyanpaul1234@users.noreply.github.com>
2023-05-11 10:26:29 +0530

    feat: updated n

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
           <Route exact path="/"> */}
           {/* </Route>
         </Switch> */}
-    {/* <About/> */}
+    <About/>
     </div>
     {/* </Router> */}
     </> 
